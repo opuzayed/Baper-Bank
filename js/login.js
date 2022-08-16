19ff3dbf41d20c7f7ec34dd3daba16f925338088
opuzayed <zayedopu4933@gmail.com>
2022-08-16 18:36:32 +0600

    added upper part

--- js/login.js
+++ js/login.js
@@ -1,13 +1,11 @@
-document.getElementById('btn-submit').addEventListener('click', function(){
-const emailField = document.getElementById('user-email');  
-const email = emailField.value;
-const passwordField = document.getElementById('user-password');
-const password = passwordField.value;
-if(email == 'opu@email.com' && password == '1234')
-{
-console.log('valid user');
-}
-else{
-    console.log('invalid user');
-}
-})
+document.getElementById("btn-submit").addEventListener("click", function () {
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+  if (email == "opu@email.com" && password == 1234) {
+    window.location.href = "bank.html";
+  } else {
+    alert("You are a invalid user");
+  }
+});
