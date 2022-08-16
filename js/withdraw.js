a82889226393b86341964fa256def67ed02abbe8
opuzayed <zayedopu4933@gmail.com>
2022-08-17 01:50:30 +0600

    add validation

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,6 +5,8 @@
 //4.calculate total withdraw amount
 //4.5-set total withdraw amount
 //5. get the previous balance total
+//6.calculate new balance total
+//6.5.set new balance total
 //7.clear the withdraw field
 
 //step-1
@@ -14,19 +16,38 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newAmountWithdrawString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newAmountWithdrawString);
+
+    //step-7
+    withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+        alert('please provide a valid number');
+        return;
+    }
    
     //step-3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
+    //step-5
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+     
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('amount is low. Try another amount');
+        return;
+    }
+
     //step-4
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
     withdrawTotalElement.innerText = currentWithdrawTotal;
 
-    //step-5
-    
+    //step-6
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 
-    //step-7
-    withdrawField.value = '';
+   
 })
