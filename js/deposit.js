cff63881e4f17eca8e7d4a49a913cfe357838df4
opuzayed <zayedopu4933@gmail.com>
2022-08-17 00:24:17 +0600

    added deposit value

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,14 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
-    console.log(depositAmount);
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    depositField.value = '';
 })
