da33984034a7b18a40570712f96ebf6a50e4e386
opuzayed <zayedopu4933@gmail.com>
2022-08-16 15:39:35 +0600

    added input field

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+const emailField = document.getElementById('user-email');  
+const email = emailField.value;
+console.log(email);
+})
