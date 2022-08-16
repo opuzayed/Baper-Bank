e910ac274415d4fd529e47a9740d397d82f2ba9a
opuzayed <zayedopu4933@gmail.com>
2022-08-16 15:52:41 +0600

    create login form

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
 const emailField = document.getElementById('user-email');  
 const email = emailField.value;
-console.log(email);
+const passwordField = document.getElementById('user-password');
+const password = passwordField.value;
+if(email == 'opu@email.com' && password == '1234')
+{
+console.log('valid user');
+}
+else{
+    console.log('invalid user');
+}
 })
