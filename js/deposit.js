629a5ec2024f97a3e06d3225e4c2715b4c35f66a
opuzayed <zayedopu4933@gmail.com>
2022-08-16 23:43:18 +0600

    added deposit field

---
+++ js/deposit.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+    console.log(depositAmount);
+})
